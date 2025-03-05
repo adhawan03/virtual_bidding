e43162c32d7c512164f5a2963056efa3b5c5db6a
adhawan03 <ajd279@cornell.edu>
2025-03-05 13:00:56 -0500

    test

--- data_processing.py
+++ data_processing.py
@@ -1,7 +1,10 @@
 import pandas as pd 
-chunk_size =10000
-csv_file = 
+file_path =r"C:\Users\dhawa\Desktop\mEng Project\virtual_bidding\RT_COMMITMENT_ZONAL\OASIS_Real_Time_Commitment_Zonal_LBMP.csv"
+rtc = pd.read_csv(file_path)
+file_path = r"C:\Users\dhawa\Desktop\mEng Project\virtual_bidding\RT_LBMP\20240301rtlbmp_zone_csv\20240301rtlbmp_zone.csv"
+rtlbmp = pd.read_csv(file_path)
+file_path = r"C:\Users\dhawa\Desktop\mEng Project\virtual_bidding\DA_LBMP\20250301damlbmp_zone_csv\20250301damlbmp_zone.csv"
+dalbmp = pd.read_csv(file_path)
 
 
-# function to get RT prices 
-def get_rt
+
