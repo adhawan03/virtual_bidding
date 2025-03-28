d5b74d62672656aff96cbd4deea0afb9bea3b9b8
adhawan03 <ajd279@cornell.edu>
2025-03-28 20:21:16 +0000

    nothing interesting- more data processing

--- data_processing.py
+++ data_processing.py
@@ -1,10 +1,10 @@
 import pandas as pd 
+import numpy as np
 file_path =r"C:\Users\dhawa\Desktop\mEng Project\virtual_bidding\RT_COMMITMENT_ZONAL\OASIS_Real_Time_Commitment_Zonal_LBMP.csv"
 rtc = pd.read_csv(file_path)
 file_path = r"C:\Users\dhawa\Desktop\mEng Project\virtual_bidding\RT_LBMP\20240301rtlbmp_zone_csv\20240301rtlbmp_zone.csv"
 rtlbmp = pd.read_csv(file_path)
 file_path = r"C:\Users\dhawa\Desktop\mEng Project\virtual_bidding\DA_LBMP\20250301damlbmp_zone_csv\20250301damlbmp_zone.csv"
 dalbmp = pd.read_csv(file_path)
-
 
 
